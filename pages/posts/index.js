993f9292f7dfbd2f1fe275b40dafce4c5a309aab
Mike McGee <mmcgee738@gmail.com>
2025-06-17 19:10:38 -0500

    Update index.js
    removed `{console.warn(posts)}` from return statement on `Home`

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -14,7 +14,6 @@ function Home() {
 
   return (
     <article className="posts">
-      {console.warn(posts)}
       <h1>All Posts</h1>
       <Button onClick={() => { router.push('posts/new'); }}>
         Create a new post
